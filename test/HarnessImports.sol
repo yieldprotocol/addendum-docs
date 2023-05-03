1eb6af6696b0fcb047dabed033350027e2506012
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-05-03 06:20:36 -0500

    update remappings

--- test/HarnessImports.sol
+++ test/HarnessImports.sol
@@ -1,33 +1,30 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.8.13;
 
-import "lib/forge-std/src/Test.sol";
+import "forge-std/Test.sol";
 
-import {CastBytes32Bytes6}      from "lib/yield-utils-v2/contracts/cast/CastBytes32Bytes6.sol";
-import {CastU256I128}           from "lib/yield-utils-v2/contracts/cast/CastU256I128.sol";
-import {CastU256U128}           from "lib/yield-utils-v2/contracts/cast/CastU256U128.sol";
-import {CastU128I128}           from "lib/yield-utils-v2/contracts/cast/CastU128I128.sol";
+import {Cast}                   from "yield-utils-v2/utils/Cast.sol";
 
-import {DataTypes}              from "lib/vault-v2/packages/foundry/contracts/interfaces/DataTypes.sol";
+import {DataTypes}              from "vault-v2/interfaces/DataTypes.sol";
 
-import {IERC20}                 from "lib/yield-utils-v2/contracts/token/IERC20.sol";
-import {IERC20Metadata}         from "lib/yield-utils-v2/contracts/token/IERC20Metadata.sol";
-import {IERC2612}               from "lib/yield-utils-v2/contracts/token/IERC2612.sol";
-import {ERC20Permit}            from "lib/yield-utils-v2/contracts/token/ERC20Permit.sol";
+import {IERC20}                 from "yield-utils-v2/token/IERC20.sol";
+import {IERC20Metadata}         from "yield-utils-v2/token/IERC20Metadata.sol";
+import {IERC2612}               from "yield-utils-v2/token/IERC2612.sol";
+import {ERC20Permit}            from "yield-utils-v2/token/ERC20Permit.sol";
 
-import {ICauldron}              from "lib/vault-v2/packages/foundry/contracts/interfaces/ICauldron.sol";
-import {IFYToken}               from "lib/vault-v2/packages/foundry/contracts/interfaces/IFYToken.sol";
-import {IJoin}                  from "lib/vault-v2/packages/foundry/contracts/interfaces/IJoin.sol";
-import {ILadle}                 from "lib/vault-v2/packages/foundry/contracts/interfaces/ILadle.sol";
-import {RepayFromLadleModule}   from "lib/vault-v2/packages/foundry/contracts/modules/RepayFromLadleModule.sol";
-import {WrapEtherModule}        from "lib/vault-v2/packages/foundry/contracts/modules/WrapEtherModule.sol";
-import {Transfer1155Module}     from "lib/vault-v2/packages/foundry/contracts/other/notional/Transfer1155Module.sol";
-import {ERC1155}                from "lib/vault-v2/packages/foundry/contracts/other/notional/ERC1155.sol";
+import {ICauldron}              from "vault-v2/interfaces/ICauldron.sol";
+import {IFYToken}               from "vault-v2/interfaces/IFYToken.sol";
+import {IJoin}                  from "vault-v2/interfaces/IJoin.sol";
+import {ILadle}                 from "vault-v2/interfaces/ILadle.sol";
+import {RepayFromLadleModule}   from "vault-v2/modules/RepayFromLadleModule.sol";
+import {WrapEtherModule}        from "vault-v2/modules/WrapEtherModule.sol";
+import {Transfer1155Module}     from "vault-v2/other/notional/Transfer1155Module.sol";
+import {ERC1155}                from "vault-v2/other/notional/ERC1155.sol";
 
-import {IPool}                  from "lib/yieldspace-tv/src/interfaces/IPool.sol";
-import {Pool}                   from "lib/yieldspace-tv/src/Pool/Pool.sol";
-import {IStrategy}              from "lib/strategy-v2/contracts/interfaces/IStrategy.sol";
-import {Strategy}               from "lib/strategy-v2/contracts/Strategy.sol";
+import {IPool}                  from "yieldspace-tv/interfaces/IPool.sol";
+import {Pool}                   from "yieldspace-tv/Pool/Pool.sol";
+import {IStrategy}              from "strategy-v2/src/interfaces/IStrategy.sol";
+import {Strategy}               from "strategy-v2/src/Strategy.sol";
 
 import {TestConstants}          from "./TestConstants.sol";
 import {TestExtensions}         from "./TestExtensions.sol";
